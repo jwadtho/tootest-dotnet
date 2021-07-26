e8da650a3ae06cd41d19749cd13e7a24d8d1b1f2
Jannarong Wadthong <toojannarong@gmail.com>
2021-07-26 19:03:30 +0700

    first commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NLog;
+
+namespace tootest_dotnet.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+
+            logger.Info("Getting WeatherForecast");
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ Dockerfile
@@ -0,0 +1,40 @@
+
+
+FROM mcr.microsoft.com/dotnet/aspnet:5.0 AS base
+WORKDIR /app
+EXPOSE 5000
+ENV ASPNETCORE_URLS=http://*:5000
+
+FROM mcr.microsoft.com/dotnet/sdk:5.0 AS build
+WORKDIR /src
+COPY ["tootest-dotnet.csproj", "./"]
+RUN dotnet restore "tootest-dotnet.csproj"
+COPY . .
+WORKDIR "/src/."
+RUN dotnet build "tootest-dotnet.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "tootest-dotnet.csproj" -c Release -o /app/publish
+
+
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+
+
+# Please select the corresponding download for your Linux containers https://github.com/DataDog/dd-trace-dotnet/releases/latest
+
+# Download and install the Tracer
+RUN mkdir -p /opt/datadog
+RUN curl -L https://github.com/DataDog/dd-trace-dotnet/releases/download/v1.28.0/datadog-dotnet-apm-1.28.0.tar.gz \
+|  ar xzf - -C /opt/datadog
+
+# Enable the tracer
+ENV CORECLR_ENABLE_PROFILING=1
+ENV CORECLR_PROFILER={846F5F1C-F9AE-4B07-969E-05C26BC060D8}
+ENV CORECLR_PROFILER_PATH=/opt/datadog/Datadog.Trace.ClrProfiler.Native.so
+ENV DD_DOTNET_TRACER_HOME=/opt/datadog
+ENV DD_INTEGRATIONS=/opt/datadog/integrations.json
+
+ENTRYPOINT ["dotnet", "tootest-dotnet.dll"]
