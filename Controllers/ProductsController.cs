37fd6c03c1df7a09411992666e7f9f192f52598e
Jannarong Wadthong <toojannarong@gmail.com>
2021-08-08 01:55:23 +0700

    add log to product controller

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using NLog;
 
 namespace tootest_dotnet.Controllers
 {
@@ -14,6 +15,8 @@ namespace tootest_dotnet.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
         private readonly Context context;
         private readonly IDbConnection dbConnection;
 
@@ -26,13 +29,16 @@ namespace tootest_dotnet.Controllers
         [HttpGet("{id}")]
         public async Task<IActionResult> Get(long id)
         {
+            logger.Info($"Getting product by id:{id}");
             var product = await this.context.Products.Where(p => p.Id == id).FirstOrDefaultAsync();
+            
             return this.Ok(product);
         }
 
         [HttpGet]
         public async Task<IActionResult> GetByName([FromQuery] string name)
         {
+            logger.Info($"Getting product by name:{name}");
             var @params = new Dictionary<string, object> { { "@name", name } };
             var products = await dbConnection.QueryAsync<object>("SELECT * FROM ch_product.product WHERE name = @name", @params);
             return this.Ok(products);
